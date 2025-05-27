7f1d4dc835b3bb838995c7c7245046b8d573c593
Facundo Marquez <130838981+FacuMarquez91@users.noreply.github.com>
2025-05-26 23:50:55 -0300

    Update ejercicio10.py

--- tp3-Cola/ejercicio10.py
+++ tp3-Cola/ejercicio10.py
@@ -17,14 +17,14 @@ cola_notificaciones = deque ([
 {"hora":"12:45","app": "instagram","mensaje":  "Que comemos?"},
 {"hora":"11:58","app": "telegram", "mensaje": "A cuanto?"},
 {"hora":"15:12","app": "facebook", "mensaje": "No voy a ir"},
-{"hora":"16:45","app": "facebook", "mensaje": "Compra una fresca"},  
+{"hora":"16:45","app": "facebook", "mensaje": "Compra algo"},  
 {"hora":"18:25","app": "telegram", "mensaje": "Ahora paso"},
 {"hora":"19:45","app": "instagram","mensaje":  "Hoy te vi!"},
-{"hora":"20:20","app": "facebook", "mensaje": "Y.. Axel es terrible"},
-{"hora":"01:15","app": "Instagram","mensaje":  "Estas para House?"},
-{"hora":"17:14","app": "twitter",  "mensaje": "sabes phyton?"},
+{"hora":"20:20","app": "facebook", "mensaje": "Y.. es terrible"},
+{"hora":"01:15","app": "Instagram","mensaje":  "Estas para un partido?"},
+{"hora":"17:14","app": "twitter",  "mensaje": "sabes python?"},
 {"hora":"11:59","app": "twiter",   "mensaje": "Mañana paso por ahí"},
-{"hora":"12:01","app": "twitter",  "mensaje": "tenes lo de phyton?"},
+{"hora":"12:01","app": "twitter",  "mensaje": "tenes lo de python?"},
 
 ])
 
@@ -55,10 +55,10 @@ mostrar_notif_twitter = deque()
 
 while cola_para_itemB:
     notificacion = cola_para_itemB.popleft()
-    if (notificacion["app"].lower() == "twitter" and "phyton" in notificacion["mensaje"].lower()):
+    if (notificacion["app"].lower() == "twitter" and "python" in notificacion["mensaje"].lower()):
         mostrar_notif_twitter.append(notificacion)
 
-print("Las notificiaciones de twitter que contienen la palabra phyton son: ")
+print("Las notificiaciones de twitter que contienen la palabra python son: ")
 pprint(mostrar_notif_twitter)
 
 
@@ -76,34 +76,6 @@ for i in cola_para_itemC:
 print("Las notificaciones que llegaron entre las 11:43 y las 15:57 son: ")
 pprint(pila_rango_horario)
 
-'''
-for i in range(len(cola_notificaciones)-1, -1, -1):
-    if cola_notificaciones[i]["aplicacion"].lower() == "facebook":
-        cola_notificaciones.pop(i)
 
-print("\n")
-print("Notificaciones luego de eliminar las de facebook")
-pprint(cola_notificaciones)
-
-print("\n")
-print("ITEM B")
-print("Las notificiaciones de twitter que nombran la palabra phyton son: ")
-for i in cola_notificaciones:
-    if i["aplicacion"].lower() == "twitter" and "phyton" in i["mensaje"]. lower():
-        pprint(i)
-
-print("\n")
-print("ITEM C")
-print("notificaciones entre las 11:43 y las 15:57" )
-
-notif_rango_horario = []
-
-for i in cola_notificaciones:
-    if i["hora"] > "11:43" and i["hora"] < "15:57":
-        notif_rango_horario.append(i)
-
-print(f"las notificiaciones entre ese horario son:  {len(notif_rango_horario)}")
-pprint(notif_rango_horario)
-'''
 
 
